5c79b70772f47e54160e68468879f46fe1c909bc
Stephen Ochieng <materiamystic@gmail.com>
2024-04-08 15:29:14 +0300

    Keeps in track with main

--- src/index.js
+++ src/index.js
@@ -7,42 +7,142 @@ document.addEventListener("DOMContentLoaded", () => {
   const ticketNum = document.querySelector("#ticket_num");
   const films = document.querySelector("ul#films");
   const buyBtn = document.querySelector("#buy_ticket");
+  const imgPoster = document.querySelector("img#poster");
 
   //load info of 1st movie upon page load
-  fetch(`http://localhost:3000/films/1`)
-    .then((res) => res.json())
-    .then((data) => {
-      docTitle.textContent = data.title;
-      runtime.textContent = data.runtime;
-      info.textContent = data.description;
-      showtime.textContent = data.showtime;
-      let remainingTickets = data.capacity - data.tickets_sold;
-      ticketNum.textContent = remainingTickets;
-    });
-
-  //Load movie titles
-  fetch("http://localhost:3000/films")
-    .then((res) => res.json())
-    .then((data) => {
-      for (const record of data) {
-        const li = document.createElement("li");
-        li.textContent = record.title;
-        li.classList.add("film", "item");
-        films.append(li);
-      }
-    });
-
-  //Buy tickets
-  buyBtn.addEventListener("click", () => {
-    ticketNum.textContent -= 1;
-    fetch("http://localhost:3000/films")
+  function movieData() {
+    fetch(`http://localhost:3000/films`)
       .then((res) => res.json())
       .then((data) => {
-        data.forEach((record) => {
-          if (docTitle.textContent === record.title) {
-            ticketsSold = record.tickets_sold + 1;
+        function firstMovie() {
+          const dataOne = data[0];
+
+          docTitle.textContent = dataOne.title;
+          runtime.textContent = dataOne.runtime;
+          info.textContent = dataOne.description;
+          showtime.textContent = dataOne.showtime;
+          let remainingTickets = dataOne.capacity - dataOne.tickets_sold;
+          ticketNum.textContent = remainingTickets;
+          imgPoster.src = dataOne.poster;
+          imgPoster.alt = dataOne.title;
+        }
+        firstMovie();
+
+        //Load movie titles
+        for (const record of data) {
+          const li = document.createElement("li");
+          const delBtn = document.createElement("button");
+          delBtn.textContent = "Delete";
+          delBtn.classList.add("block", "delBtn");
+          li.textContent = `${record.title}  `;
+          li.appendChild(delBtn);
+          li.classList.add("film", "item");
+          films.appendChild(li);
+        }
+
+        //Buy tickets
+        let ticketsSold;
+        let t = ticketNum.textContent; //saves or tracks original number before clicking on buy ticket button
+        const filmsArr = document.querySelectorAll("li");
+
+        buyBtn.addEventListener("click", () => {
+          if (ticketNum.textContent > 0) {
+            // Inside the event listener, checks if the ticket can be bought
+            ticketNum.textContent -= 1;
+            let numberOfTicketsSold = t - ticketNum.textContent;
+            // Access record and update ticketsSold and movieId
+            data.forEach((record) => {
+              if (docTitle.textContent === record.title) {
+                ticketsSold = record.tickets_sold + numberOfTicketsSold; //Adds the difference to the earlier retrieved record of tickets sold.
+                let movieId = record.id;
+
+                updateTicketsSold(movieId, ticketsSold); //tickets sold is persisted to the server
+                postNewTicket(movieId, numberOfTicketsSold);
+              }
+            });
+          } else {
+            buyBtn.textContent = "sold-out";
+
+            filmsArr.forEach((film) => {
+              if (film.textContent.includes(docTitle.textContent)) {
+                film.classList.add("sold-out");
+              }
+            });
           }
         });
-      });
-  });
+
+        const deleteBtns = films.querySelectorAll("button");
+        deleteBtns.forEach((item) => {
+          item.addEventListener("click", (e) => {
+            e.target.parentNode.remove();
+            data.forEach((record) => {
+              if (
+                e.target.parentNode.textContent.replace("  Delete", "") ===
+                record.title
+              ) {
+                let movieId = record.id;
+                handleDelete(movieId);
+              }
+            });
+          });
+        });
+
+        films.addEventListener("click", (e) => {
+          let movieTitle = e.target.textContent.replace("  Delete", "");
+
+          data.forEach((item) => {
+            if (item.title === movieTitle) {
+              docTitle.textContent = item.title;
+              runtime.textContent = item.runtime;
+              info.textContent = item.description;
+              showtime.textContent = item.showtime;
+              let remainingTickets = item.capacity - item.tickets_sold;
+              ticketNum.textContent = remainingTickets;
+              imgPoster.src = item.poster;
+              imgPoster.alt = item.title;
+              t = ticketNum.textContent; //resets value of t to the selected movie's value
+            }
+          });
+          // console.log(data[0].title);
+        });
+      }); //data
+  } //function movieData
+
+  movieData();
+
+  //PATCH request function
+  function updateTicketsSold(movie_Id, soldTickets) {
+    fetch(`http://localhost:3000/films/${movie_Id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ tickets_sold: soldTickets }),
+    });
+    // .then((res) => res.json())
+    // .then((patchResponse) => console.log(patchResponse));
+  }
+
+  //POST request function
+  function postNewTicket(movie_Id, no_tickets) {
+    const postObj = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        film_id: movie_Id,
+        number_of_tickets: no_tickets,
+      }),
+    };
+    fetch("http://localhost:3000/tickets/", postObj);
+  }
+
+  //DELETE request function
+  function handleDelete(movie_Id) {
+    fetch(`http://localhost:3000/films/${movie_Id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 });
